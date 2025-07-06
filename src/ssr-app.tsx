aa5bdd88a522f39e2ff9007c1ba9649395e211bf
ThomasChant <xgct0505@qq.com>
2025-07-06 23:08:35 +0800

    移除不再使用的ClientOnly组件导入，简化SSR应用结构

--- src/ssr-app.tsx
+++ src/ssr-app.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ClerkProvider } from "@clerk/clerk-react";
 import Layout from "./components/Layout/Layout";
-import { ClientOnly } from "./components/ClientOnly";
 import Home from "./pages/Home";
 import ServersPage from "./pages/Servers";
 import ServerDetailPage from "./pages/ServerDetail";
